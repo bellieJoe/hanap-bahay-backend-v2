5c9e91366751e802ee152e2d89aa35f5cc8b484d
Bellie Joe M. Jandusay <jandusayjoe14@gmail.com>
2022-04-05 11:51:08 +0800

    asda

--- app/Http/Controllers/RRPTypeController.php
+++ app/Http/Controllers/RRPTypeController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\RRPType;
 use Illuminate\Http\Request;
 
 class RRPTypeController extends Controller
 {
-    //
+    public function countRRPTypes($rrpId) {
+        $count = RRPType::where([
+            'RRP_ID' => $rrpId
+        ])->count();
+
+        return json_encode($count);
+    }   
 }
